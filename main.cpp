06b2d35b762eda7520da7b094548b7c27621ff39
candrowm <martin.candrowicz@web.de>
2017-11-07 11:41:25 +0100

    Für jeden Client 1 Thread, der fürs Schreiben zuständig ist

--- main.cpp
+++ main.cpp
@@ -1,77 +1,91 @@
 #include <iostream>
-
-#include<stdio.h>
-#include<string.h>    //strlen
+#include<string.h>
 #include<sys/socket.h>
-#include<arpa/inet.h> //inet_addr
-#include<unistd.h>    //write
-
+#include<arpa/inet.h>
+#include<unistd.h>
 #include <thread>
-
-#include <stdio.h>
-#include <string.h>   //strlen
-#include <stdlib.h>
-#include <errno.h>
-#include <unistd.h>   //close
-#include <arpa/inet.h>    //close
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
-
 #include <mutex>
 
-#define TRUE   1
-#define FALSE  0
 #define PORT 9999
+#define MAXTHREADS 50
 
-int opt = TRUE;
-int master_socket , addrlen , new_socket , client_socket[30] , max_clients = 30 , activity, i , valread , sd;
-int max_sd;
-struct sockaddr_in address;
-char buffer[1025];  //data buffer of 1K
 
-std::mutex mtx;
+int threadFileDescriptors[MAXTHREADS] = {};
+pthread_cond_t threadConditions[MAXTHREADS] = {};
+pthread_mutex_t threadMutexes[MAXTHREADS] = {};
 
-void sendMessageToAnUser(int sd) {
-    send(sd , buffer , strlen(buffer) , 0 );
+
+char buffer[1025];  //data buffer of 1
+
+
+struct arg_struct {
+    int fd;
+};
+
+
+int findPositionInArray(int val, int *arr, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (arr[i] == val)
+            return i;
+    }
+    return -1;
 }
 
-void sendMessageToAllUsers(char buffer[]) {
-    for(int i = 0; i < max_clients; i++){
-        sd = client_socket[i];
+void *socketThread(void *arguments) {
+    struct arg_struct *args = (struct arg_struct *) arguments;
 
-        //if valid socket descriptor then send message
-        if(sd > 0){
-            std::thread messageToAnUserThread(sendMessageToAnUser,sd);
-            messageToAnUserThread.join();
-            // sendMessageToAnUser(sd);
+    std::cout << "New Thread with fd: " << args->fd << "\n";
 
+    int threadFileDescriptor = args->fd;
+
+    int positionOfClient = findPositionInArray(threadFileDescriptor, threadFileDescriptors, MAXTHREADS);
+
+
+    while (true) {
+        pthread_mutex_lock(&threadMutexes[positionOfClient]);
+        pthread_cond_wait(&threadConditions[positionOfClient], &threadMutexes[positionOfClient]);
+        // printf("Got SocketThread Cond \n");
+        pthread_mutex_unlock(&threadMutexes[positionOfClient]);
+
+        try {
+            send(threadFileDescriptor, buffer, strlen(buffer), 0);
+        } catch (std::string exception) {
+            threadFileDescriptors[positionOfClient] = 0;
+            pthread_exit(NULL);
         }
     }
-    std::string emptyString = "";
-    strcpy(buffer, emptyString.c_str());
+
 }
 
 
-int serverListen()
-{
+void writeMessageToAllUsers(std::string buffer) {
 
-    //set of socket descriptors
+    for (int i = 0; i < MAXTHREADS + 1; i++) {
+        if (threadFileDescriptors[i] > 0) {
+            pthread_mutex_lock(&threadMutexes[i]);
+            // printf("SocketThread Cond fire\n");
+            pthread_cond_signal(&threadConditions[i]);
+            pthread_mutex_unlock(&threadMutexes[i]);
+        }
+    }
+}
+
+void *masterSocketThread(void *ptr) {
+    int master_socket, addrlen, new_socket, activity, valread, sd;
     fd_set readfds;
-
-    //a message
-    char *message = "ECHO Daemon v1.0 \r\n";
+    int max_sd;
+    struct sockaddr_in address;
+    char *message = "Welcome!\n";
 
     //initialise all client_socket[] to 0 so not checked
-    for (i = 0; i < max_clients; i++)
-    {
-        client_socket[i] = 0;
+    for (int i = 0; i < MAXTHREADS; i++) {
+        threadFileDescriptors[i] = 0;
     }
 
+
     //create a master socket
-    if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
-    {
+    if ((master_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
@@ -81,29 +95,26 @@ int serverListen()
     //type of socket created
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons(PORT);
 
     //bind the socket to localhost port 8888
-    if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
-    {
+    if (bind(master_socket, (struct sockaddr *) &address, sizeof(address)) < 0) {
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
     printf("Listener on port %d \n", PORT);
 
     //try to specify maximum of 3 pending connections for the master socket
-    if (listen(master_socket, 3) < 0)
-    {
+    if (listen(master_socket, 3) < 0) {
         perror("listen");
         exit(EXIT_FAILURE);
     }
 
     //accept the incoming connection
     addrlen = sizeof(address);
-    puts("Waiting for connections ...");
+    puts("Wait for connections ...");
 
-    while(TRUE)
-    {
+    for (;;) {
         //clear the socket set
         FD_ZERO(&readfds);
 
@@ -112,217 +123,154 @@ int serverListen()
         max_sd = master_socket;
 
         //add child sockets to set
-        for ( i = 0 ; i < max_clients ; i++)
-        {
+        for (int i = 0; i < MAXTHREADS; i++) {
             //socket descriptor
-            sd = client_socket[i];
+            sd = threadFileDescriptors[i];
 
             //if valid socket descriptor then add to read list
-            if(sd > 0)
-                FD_SET( sd , &readfds);
+            if (sd > 0)
+                FD_SET(sd, &readfds);
 
             //highest file descriptor number, need it for the select function
-            if(sd > max_sd)
+            if (sd > max_sd)
                 max_sd = sd;
         }
 
         //wait for an activity on one of the sockets , timeout is NULL , so wait indefinitely
-        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+        activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
 
-        if ((activity < 0) && (errno!=EINTR))
-        {
+
+        if ((activity < 0) && (errno != EINTR)) {
             printf("select error");
         }
 
         //If something happened on the master socket , then its an incoming connection
-        // --> es gibt was beim master socket zu lesen, da neue Nachricht
-        if (FD_ISSET(master_socket, &readfds))
-        {
-            if ((new_socket = accept(master_socket, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
-            {
+        if (FD_ISSET(master_socket, &readfds)) {
+            if ((new_socket = accept(master_socket, (struct sockaddr *) &address, (socklen_t *) &addrlen)) < 0) {
                 perror("accept");
                 exit(EXIT_FAILURE);
             }
 
             //inform user of socket number - used in send and receive commands
-            printf("New connection , socket fd is %d , ip is : %s , port : %d \n" , new_socket , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+            printf("New connection , socket fd is %d , ip is : %s , port : %d \n", new_socket,
+                   inet_ntoa(address.sin_addr), ntohs(address.sin_port));
 
             //send new connection greeting message
-            if( send(new_socket, message, strlen(message), 0) != strlen(message) )
-            {
+            if (send(new_socket, message, strlen(message), 0) != strlen(message)) {
                 perror("send");
             }
 
-            puts("Welcome message sent successfully");
 
             //add new socket to array of sockets
-            for (i = 0; i < max_clients; i++)
-            {
+            for (int i = 0; i < MAXTHREADS; i++) {
                 //if position is empty
-                if( client_socket[i] == 0 )
-                {
-                    client_socket[i] = new_socket;
-                    printf("Adding to list of sockets as %d\n" , i);
+                if (threadFileDescriptors[i] == 0) {
+
+                    //printf("Adding new socket to list %d\n", i);
+
+                    pthread_mutex_t new_mutex = PTHREAD_MUTEX_INITIALIZER;
+                    pthread_cond_t new_cond = PTHREAD_COND_INITIALIZER;
+
+                    struct arg_struct args;
+                    args.fd = new_socket;
+
+                    threadFileDescriptors[i] = new_socket;
+                    threadMutexes[i] = new_mutex;
+                    threadConditions[i] = new_cond;
+
+                    pthread_t clientSocketThread;
+                    int err = pthread_create(&clientSocketThread, NULL, socketThread, &args);
 
                     break;
                 }
             }
-        }
-
-        //else its some IO operation on some other socket :)
-        // --> Es gibt was zu lesen: Entweder neue Nachricht von einem Client oder ein Client ist disconected
-
-        bool newMessage=false;
-
-        for (i = 0; i < max_clients; i++)
-        {
-            sd = client_socket[i];
-
-            if (FD_ISSET( sd , &readfds))
-            {
-                //Check if it was for closing , and also read the incoming message
-                if ((valread = read( sd , buffer, 1024)) == 0)
-                {
-                    //Somebody disconnected , get his details and print
-                    getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);
-                    //printf("Host disconnected , ip %s , port %d \n" , inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
-
-                    std::string ipFromSender = inet_ntoa(address.sin_addr);
-                    std::string portFromSender = std::to_string(ntohs(address.sin_port));
-                    std::string messageFromSender = ipFromSender+":"+portFromSender+" hat den Chat verlassen";
-
-                    strcpy(buffer, messageFromSender.c_str());
-                    std::cout << buffer << "\n";
-
-                    //Close the socket and mark as 0 in list for reuse
-                    close( sd );
-                    client_socket[i] = 0;
-                }
-
-                    //Echo back the message that came in
-                else
-                {
-                    getpeername(sd , (struct sockaddr*)&address , (socklen_t*)&addrlen);
-                    buffer[valread] = '\0';
-                    // std::cout << inet_ntoa(address.sin_addr) <<":"<< ntohs(address.sin_port) << " schrieb: " << buffer << "\n";
 
 
-                    std::string ipFromSender = inet_ntoa(address.sin_addr);
-                    std::string portFromSender = std::to_string(ntohs(address.sin_port));
+        } else
+            //else its some IO operation on some other socket :) --> New message from client or cliebt disconnected
+            for (int i = 0; i < MAXTHREADS; i++) {
+                sd = threadFileDescriptors[i];
 
-                    ;                   std::string messageFromSender = ipFromSender+":"+portFromSender+ " schrieb:" + buffer+ " \n";
+                if (FD_ISSET(sd, &readfds)) {
+                    //Check if it was for closing , and also read the incoming message
+                    if ((valread = read(sd, buffer, 1024)) == 0) {
+                        //Somebody disconnected , get his details and print
+                        getpeername(sd, (struct sockaddr *) &address, (socklen_t *) &addrlen);
 
-                    strcpy(buffer, messageFromSender.c_str());
-
-                    //mtx.lock();
-                    std::cout << buffer << "\n";
-                    // mtx.unlock();
+                        std::string ipFromSender = inet_ntoa(address.sin_addr);
+                        std::string portFromSender = std::to_string(ntohs(address.sin_port));
+                        std::string messageFromSender = ipFromSender + ":" + portFromSender + " disconnected \n";
 
 
+                        strcpy(buffer, messageFromSender.c_str());
+                        std::cout << buffer;
 
-                    // send(sd , buffer , strlen(buffer) , 0 );
+                        //Close the socket and mark as 0 in list for reuse
+                        close(sd);
+                        threadFileDescriptors[i] = 0;
+
+                        writeMessageToAllUsers(buffer);
+                    }
+                        //Echo back the message that came in
+                    else {
+                        getpeername(sd, (struct sockaddr *) &address, (socklen_t *) &addrlen);
+                        buffer[valread] = '\0';
+
+                        std::string ipFromSender = inet_ntoa(address.sin_addr);
+                        std::string portFromSender = std::to_string(ntohs(address.sin_port));
+
+                        std::string messageFromSender = ipFromSender + ":" + portFromSender + ": " + buffer;
+
+                        strcpy(buffer, messageFromSender.c_str());
+
+                        std::cout << buffer;
+                        writeMessageToAllUsers(buffer);
+                    }
                 }
             }
-        }
-        if(strlen(buffer)>0){
-            sendMessageToAllUsers(buffer);
-        }
-
     }
-
-    return 0;
 }
 
 
-
-
-void serverChat()
-{
-    while(true){
-        std::string vielleicht= "";
-        //std::cin >> vielleicht;
-        //std::cout << "EINGABE WAR: :" << vielleicht;
+void *serverChatThread(void *ptr) {
+    while (true) {
+        std::string serverMessage = "";
 
         //mtx.lock();
-        std::getline(std::cin,vielleicht);
+        std::getline(std::cin, serverMessage);
 
-        std::string ipFromSender = "SERVER";
-        std::string portFromSender = "PORT" ;
-        std::string messageFromSender = ipFromSender+":"+portFromSender+ " schrieb:" + vielleicht + " \n";
+        std::string portOfServer = std::to_string(PORT);
+
+        std::string ipFromSender = "Server";
+        std::string portFromSender = PORT + "";
+        std::string messageFromSender = ipFromSender + ":" + portOfServer + ": " + serverMessage + " \n";
 
         strcpy(buffer, messageFromSender.c_str());
-        std::cout << buffer << "\n";
-        // mtx.unlock();
+        //std::cout << buffer << "\n";
 
-        if(strlen(buffer)>0){
-            sendMessageToAllUsers(buffer);
+        if (strlen(buffer) > 0) {
+            writeMessageToAllUsers(buffer);
         }
     }
-
-
-}
-
-int client(){
-    int sock;
-    struct sockaddr_in server;
-    char message[1000] , server_reply[2000];
-
-    //Create socket
-    sock = socket(AF_INET , SOCK_STREAM , 0);
-    if (sock == -1)
-    {
-        printf("Could not create socket");
-    }
-    puts("Socket created");
-
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_family = AF_INET;
-    server.sin_port = htons( 8888 );
-
-    //Connect to remote server
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
-        return 1;
-    }
-
-    puts("Connected\n");
-
-    //keep communicating with server
-    while(1)
-    {
-        printf("Enter message : ");
-        scanf("%s" , message);
-
-        //Send some data
-        if( send(sock , message , strlen(message) , 0) < 0)
-        {
-            puts("Send failed");
-            return 1;
-        }
-
-        //Receive a reply from the server
-        if( recv(sock , server_reply , 2000 , 0) < 0)
-        {
-            puts("recv failed");
-            break;
-        }
-
-        puts("Server reply :");
-        puts(server_reply);
-    }
-
-    close(sock);
-    return 0;
-
 }
 
 
 
-int main(int argc , char *argv[]){
-    std::thread serverListenThread(serverListen);
-    std::thread serverChatThread(serverChat);
-    serverListenThread.join();
-    serverChatThread.join();
+int main(int argc, char *argv[]) {
+
+    pthread_t serverSocketThread;
+    int errorServerSocketThread = pthread_create(&serverSocketThread, NULL, masterSocketThread, NULL);
+
+    pthread_t chatServerThread;
+    int errorChatServerThread = pthread_create(&chatServerThread, NULL, serverChatThread, NULL);
+
+    pthread_join(serverSocketThread, NULL);
+
+    if (errorServerSocketThread != 0 || errorChatServerThread != 0) {
+        printf("\ncan't create thread :[%s]", strerror(errorChatServerThread));
+    } else {
+        printf("\n Thread created successfully\n");
+    }
+
     return 0;
 }
